fae0eadbe5303588c99c034e03740d4b539bc29c
papasani-soft <papasani.soft@gmail.com>
2018-10-20 22:49:04 +0530

    commit

---
+++ collection/collection/collection/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> points = new Dictionary<string, int>
+            {
+                { "James", 9001 },
+                { "Jo", 3474 },
+                { "Jess", 11926 }
+            };
+            points.Add("abcd", 89);
+            points.Add("BCA", 2);
+            points.Add("BTech", 3);
+            points.Add("MTech", 4);
+            foreach (KeyValuePair<string, int> pair in points)
+            {
+                Console.WriteLine("{0}, {1}", pair.Key, pair.Value);
+            }
+
+            //REMOVE the element
+            points.Remove("BTech");
+            points.Remove("BCA");
+            Console.WriteLine("after removal the dictionary items are:");
+
+            foreach (KeyValuePair<string, int> pair in points)
+            {
+                Console.WriteLine("{0}, {1}", pair.Key, pair.Value);
+            }
+            FileStream fs = new FileStream("F://asmt//serial_demo.txt", FileMode.OpenOrCreate);
+            BinaryFormatter bfm = new BinaryFormatter();
+         
+            bfm.Serialize(fs, points);
+            fs.Close();
+            Console.WriteLine("Serialization completed");
+
+            string json = JsonConvert.SerializeObject(points, Formatting.Indented);
+
+            Console.WriteLine(json);
+            Console.Read();
+        }
+    }
+}
---
+++ collection/collection/collection/Program2.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collection
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            var con = new ConcurrentDictionary<string, int>();
+            con.TryAdd("cat", 1);
+            con.TryAdd("dog", 2);
+            con.TryUpdate("cat", 100, 1);
+            Console.WriteLine(con["cat"]);
+
+            con.AddOrUpdate("dog", 5, (k, v) => v + 1);
+
+            // Display dog value.
+            Console.WriteLine(con["dog"]);
+
+            // Get mouse or add it with value of 4.
+            int mouse = con.GetOrAdd("mouse", 4);
+            Console.WriteLine(mouse);
+
+            // Get mouse or add it with value of 660.
+            mouse = con.GetOrAdd("mouse", 660);
+            Console.WriteLine(mouse);
+
+
+
+            FileStream fs = new FileStream("F://asmt//serial_demo.txt", FileMode.OpenOrCreate);
+            BinaryFormatter bfm = new BinaryFormatter();
+
+            bfm.Serialize(fs, con);
+            fs.Close();
+            Console.WriteLine("Serialization completed");
+
+            string json = JsonConvert.SerializeObject(con, Formatting.Indented);
+
+            Console.WriteLine(json);
+            Console.Read();
+        }
+    }
+}
